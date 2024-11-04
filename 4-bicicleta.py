4dd25186a6faf28faf49c4d0f3f3f4eba0ddb6b5
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-11-03 23:09:07 -0300

    Atividade-4

--- 4-bicicleta.py
+++ 4-bicicleta.py
@@ -5,9 +5,16 @@ class Bicicleta:
     altura_cela = None
     calibragem_pneus = None
     
-    def __init__(self):
+    def __init__(self, veloc_atual, altura_cela, calibragem_pneus):
+        self.veloc_atual = float(veloc_atual)
+        self.altura_cela = float(altura_cela)
+        calibragem_pneus = float(calibragem_pneus)
+
+    def regular_cela(self):
         pass
-    
+
+    def calibrar_pneus(self): 
+        pass
 
 def main():
     pass
