c05c9a88ae6e7126e87fdadd8d0e7696e8a1aff0
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-11-03 23:00:05 -0300

    Atividade-4

---
+++ 4-bicicleta.py
@@ -0,0 +1,16 @@
+# Implementar a classe Bicicleta, colocando limites máximos e mínimos para os estados: veloc_atual, altura_cela e calibragem_pneus através de seus respectivos métodos. Alterar os métodos: regular_cela, calibrar_pneus para permitirem as mudanças caso a bicicleta esteja parada (veloc_atual=0).
+
+class Bicicleta:
+    veloc_atual = None
+    altura_cela = None
+    calibragem_pneus = None
+    
+    def __init__(self):
+        pass
+    
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
