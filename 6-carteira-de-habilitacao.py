9baec2eb4517b72f68a5cd5d90777c1d77612311
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-11-12 19:36:55 -0300

    Atividade-6

---
+++ 6-carteira-de-habilitacao.py
@@ -0,0 +1,4 @@
+# Utilizando o processo de abstração, implemente uma classe em python que represente uma
+# carteira de habilitação. Identifique atributos mutáveis e imutáveis, implemente um construtor
+# da classe e métodos para manipulação dos atributos mutáveis. Faça todas as validações
+# possíveis. Crie objetos para testar os métodos implementados.
