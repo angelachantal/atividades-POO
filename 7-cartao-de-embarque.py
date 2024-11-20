5f939628dcd2c7bd0303fef08e0ff538d4814871
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-11-20 16:59:18 -0300

    7-cartao-de-embarque

---
+++ 7-cartao-de-embarque.py
@@ -0,0 +1,2 @@
+# Utilizando o processo de abstração, implemente uma classe em Python que represente um cartão de embarque de voo. Identifique atributos mutáveis e imutáveis, implemente um construtor da classe e métodos para manipulação dos atributos mutáveis. Faça todas as validações possíveis. Crie objetos para testar os métodos implementados.
+
