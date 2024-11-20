7560e2333005d5cf7a5819881c7ebd24a3bfb5f3
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-11-20 18:19:04 -0300

    7-cartao-de-embarque_1

--- 7-cartao-de-embarque.py
+++ 7-cartao-de-embarque.py
@@ -2,8 +2,15 @@
 from datetime import date
 
 class CartaoEmbarque:
-    def __init__(self, nome, voo, localizador, data_emb, hora_emb, status = 'Check-in não realizado', assento = 'Aleatório') -> None:
-        pass
+    # Método construtor/inicializador:
+    def __init__(self, nome, voo, localizador, data_emb, hora_emb, status = 'Check-in não realizado', assento = 'Aleatório'):
+        self.nome = nome
+        self.voo = voo
+        self.localizador = localizador
+        self.data_emb = data_emb
+        self.hora_emb = hora_emb
+        self.status = status
+        self.assento = assento
     
     def check_in (self):
         # altera o status e associa um assento (aleatório) disponível ao passageiro.
@@ -16,14 +23,22 @@ class CartaoEmbarque:
         # Requisitos de validação: o código da reserva deve ter 6 caracteres alfanuméricos; a hora do embarque não pode ser retroativa em relação ao momento de execução do código.
         pass
     def __str__(self) -> str:
-        pass
+        return (f'Passageiro: {self.nome}\n'
+                f'Vôo: {self.voo}\n'
+                f'Localizador: {self.localizador}\n' 
+                f'Data de Embarque: {self.data_emb}\n'
+                f'Hora de Embarque: {self.hora_emb}\n'
+                f'Status: {self.status}\n'
+                f'Assento: {self.assento}\n'
+                )
     
 def main():
-    passageiro1 = CartaoEmbarque()
-    passageiro2 = CartaoEmbarque()
-    passageiro3 = CartaoEmbarque()
-    pass
-
+    passageiro1 = CartaoEmbarque('Ângela Nunes', '235', 'IHHH3M', '24/11/2024', '08:56')
+    passageiro2 = CartaoEmbarque('Roberto Lemos', '235', 'OMNH5I', '24/11/2024', '08:56')
+    passageiro3 = CartaoEmbarque('Denise Pereira', '235', 'AOMF9O', '24/11/2024', '08:56')
+    print(passageiro1)
+    print(passageiro2)
+    print(passageiro3)
+    
 if __name__=='__main__':
     main()
-    
