91476853240b57da58de07a11c387d163f0f066a
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-12-06 19:56:44 -0300

    Add files via upload

---
+++ 10-ar-condicionado.py
@@ -0,0 +1,24 @@
+# Implementar no código tudo que foi visto até agora na disciplina: Construtores, atributos opcionais, encapsulamento, ...)
+# Fazer as validações necessárias
+# Requisitos:
+    # 1. Classe ArCondicionado
+        # o atributos:
+        # ▪ temp_min:
+        # ▪ temp_max:
+        # ▪ vel_min:
+        # ▪ vel_max:
+        # ▪ temperatura: <temperatura_escolhida dentro dos limites>
+        # ▪ velocidade: <velocidade escolhida dentro dos limites>
+        # ▪ modo: <’automático’/’frio’/’ventilar’>
+        # ▪ ligado: True/False
+    # 2. Métodos a serem implementados:
+        # o ligar() :
+        # o desligar() :
+        # o aumentar_temperatura() :
+        # o diminuir_temperatura() :
+        # o aumentar_velocidade() :
+        # o diminuir_velocidade() :
+        # o mudar_modo(modo) : No modo automático, não é permitido alterar a
+        # velocidade
+
+    # 3. Crie pelo menos 3 objetos, imprima seus estados iniciais, execute os métodos e depois imprima seus estados finais.
