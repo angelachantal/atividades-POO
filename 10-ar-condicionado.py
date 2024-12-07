a64ef1874c2fecf7bedf2eca319e2d4f35070dd7
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-12-06 23:49:42 -0300

    Casos de uso

--- 10-ar-condicionado.py
+++ 10-ar-condicionado.py
@@ -1,24 +1,98 @@
-# Implementar no código tudo que foi visto até agora na disciplina: Construtores, atributos opcionais, encapsulamento, ...)
-# Fazer as validações necessárias
-# Requisitos:
-    # 1. Classe ArCondicionado
-        # o atributos:
-        # ▪ temp_min:
-        # ▪ temp_max:
-        # ▪ vel_min:
-        # ▪ vel_max:
-        # ▪ temperatura: <temperatura_escolhida dentro dos limites>
-        # ▪ velocidade: <velocidade escolhida dentro dos limites>
-        # ▪ modo: <’automático’/’frio’/’ventilar’>
-        # ▪ ligado: True/False
-    # 2. Métodos a serem implementados:
-        # o ligar() :
-        # o desligar() :
-        # o aumentar_temperatura() :
-        # o diminuir_temperatura() :
-        # o aumentar_velocidade() :
-        # o diminuir_velocidade() :
-        # o mudar_modo(modo) : No modo automático, não é permitido alterar a
-        # velocidade
+# Exercício 10 – Implementação da Classe Ar-Condicionado
 
-    # 3. Crie pelo menos 3 objetos, imprima seus estados iniciais, execute os métodos e depois imprima seus estados finais.
+class ArCondicionado():
+    # Atributos:
+            # temp_min:
+            # temp_max:
+            # vel_min:
+            # vel_max:
+            # temperatura: <temperatura_escolhida dentro dos limites>
+            # velocidade: <velocidade escolhida dentro dos limites>
+            # modo: <’automático’/’frio’/’ventilar’>
+            # ligado: True/False
+    def __init__(self) -> None:
+        pass
+    
+    def __str__(self) -> str:
+        pass
+    
+    # Métodos a serem implementados (CASOS DE USO):
+    def ligar():
+        pass
+        # Fluxo normal: 
+            # Sistema liga o aparelho
+            # Sistema imprime “Aparelho ligado”
+            # Sistema imprime estado atual
+        # Extensões: 
+            # 1a. Se o aparelho estiver ligado, imprimir “Aparelho já está ligado” e imprimir o estado atual do aparelho
+    def desligar():
+        pass       
+        # Fluxo normal: 
+            # Sistema desliga o aparelho
+            # Sistema imprime “Aparelho desligado”
+            # Sistema imprime estado atual
+        # Extensões: 
+            # 1a. Se o aparelho estiver desligado, imprimir “Aparelho já está desligado” e imprimir o estado atual do aparelho 
+    def aumentar_temperatura():
+        pass
+        # Fluxo normal: 
+            # Sistema pergunta quantos graus deseja aumentar.
+            # Usuário informa a quantidade de graus.
+            # Sistema incrementa a temperatura na quantidade de graus informada.
+            # Sistema incrementa a temperatura em 1°C.
+            # Sistema imprime estado atual.
+        # Extensões: 
+            # 1a. Se a temperatura for maior que 28°C, imprimir “Atingiu temperatura máxima” e imprimir o estado atual do aparelho.
+    def diminuir_temperatura():
+        pass
+    # Fluxo normal: 
+# Sistema decrementa a temperatura em 1°C.
+# Sistema imprime estado atual.
+# Extensões: 
+# 1a. Se a temperatura for menor que 18°C, imprimir “Atingiu temperatura mínima” e imprimir o estado atual do aparelho.
+    def aumentar_velocidade():
+        pass
+    # Fluxo normal: 
+# Sistema incrementa a velocidade em 1.
+# Sistema imprime estado atual.
+# Extensões: 
+# 1a. Se o modo for automático, imprime: “Não é possível alterar a velocidade” e imprimir o estado atual do aparelho.
+# 1b. Se a velocidade for maior que 5, imprimir “Atingiu a velocidade máxima” e imprimir o estado atual do aparelho.
+    def diminuir_velocidade():
+        pass
+    # Fluxo normal: 
+# Sistema decrementa a velocidade em 1.
+# Sistema imprime estado atual.
+# Extensões: 
+# 1a 1a. Se o modo for automático, imprime: “Não é possível alterar a velocidade” e imprimir o estado atual do aparelho.
+# 1b Se a velocidade for menor que 1, imprimir “Atingiu a velocidade mínima” e imprimir o estado atual do aparelho.
+    def mudar_modo(modo):
+        pass
+    # Fluxo normal: 
+# Sistema pergunta qual modo o usuário deseja e apresenta as opções.
+# Sistema altera o modo do aparelho
+# Sistema imprime estado atual.
+# Extensões: 
+# 2a. Se o cliente escolher a opção 1, imprime “Frio”.
+# 3b. Se o cliente escolher a opção 2, imprime “Ventilar”.
+# 3c. Se o cliente escolher a opção 3, imprime “Automático”
+# 3d. Se o cliente escolher a opção 4, imprime “Opção inválida”.
+def main():
+    pass
+# Imprimir estado
+    # Fluxo normal: 
+        # Sistema lê o estado atual do aparelho
+        # Sistema imprime o estado atual do aparelho
+        # Sistema pergunta o que o usuário deseja fazer e apresenta opções.
+    # Extensões: 
+        # 3a. Se o cliente escolher a opção 1, encerrar o programa.
+        # 3b. Se o cliente escolher a opção 2, ligar o aparelho.
+        # 3c. Se o cliente escolher a opção 3, desligar o aparelho
+        # 3d. Se o cliente escolher a opção 4, aumentar a temperatura.
+        # 3e. Se o cliente escolher a opção 5, diminuir a temperatura
+        # 3f. Se o cliente escolher a opção 6, aumentar a velocidade.
+        # 3g. Se o cliente escolher a opção 7, diminuir a velocidade.
+        # 3h. Se o cliente escolher a opção 8, mudar o modo.
+
+if __name__='__main__':
+    main()
