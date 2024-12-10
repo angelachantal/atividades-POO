1c746b5e8dc12d2f0990896593cacb7a2d3d2551
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-12-10 19:18:47 -0300

    Update 10-ar-condicionado.py

--- 10-ar-condicionado.py
+++ 10-ar-condicionado.py
@@ -2,7 +2,7 @@
 
 class ArCondicionado:
     # Método inicializador:
-    def __init__(self, modo='automático', ligado=True):
+    def __init__(self, modo='Automático', ligado=False):
         self.temperatura= 20
         self.velocidade = 3
         self.modo = modo
@@ -10,90 +10,121 @@ class ArCondicionado:
     
     def __str__(self) -> str:
         if self.ligado == True:
-            return f'APARELHO LIGADO\nModo: {self.modo}\nTemperatura: {self.temperatura}\nVelocidade: {self.velocidade}'
+            return f'APARELHO LIGADO\nModo: {self.modo}\nTemperatura: {self.temperatura}°C\nVelocidade: {self.velocidade}\n'
         else:
-            return f'APARELHO DESLIGADO'
+            return f'APARELHO DESLIGADO\n'
        
-    def ligar():
-        pass
-        # Fluxo normal: 
-            # Sistema liga o aparelho
-            # Sistema imprime “Aparelho ligado”
-            # Sistema imprime estado atual
-        # Extensões: 
-            # 1a. Se o aparelho estiver ligado, imprimir “Aparelho já está ligado” e imprimir o estado atual do aparelho
-    def desligar():
-        pass       
-        # Fluxo normal: 
-            # Sistema desliga o aparelho
-            # Sistema imprime “Aparelho desligado”
-            # Sistema imprime estado atual
-        # Extensões: 
-            # 1a. Se o aparelho estiver desligado, imprimir “Aparelho já está desligado” e imprimir o estado atual do aparelho 
-    def aumentar_temperatura():
-        pass
-        # Fluxo normal: 
-            # Sistema pergunta quantos graus deseja aumentar.
-            # Usuário informa a quantidade de graus.
-            # Sistema incrementa a temperatura na quantidade de graus informada.
-            # Sistema incrementa a temperatura em 1°C.
-            # Sistema imprime estado atual.
-        # Extensões: 
-            # 1a. Se a temperatura for maior que 28°C, imprimir “Atingiu temperatura máxima” e imprimir o estado atual do aparelho.
-            # temp_min
-            # temp_max
-    def diminuir_temperatura():
-        pass
-        # Fluxo normal: 
-            # Sistema decrementa a temperatura em 1°C.
-            # Sistema imprime estado atual.
-        # Extensões: 
-            # 1a. Se a temperatura for menor que 18°C, imprimir “Atingiu temperatura mínima” e imprimir o estado atual do aparelho.
-    def aumentar_velocidade():
-        pass
-        # Fluxo normal: 
-            # Sistema incrementa a velocidade em 1.
-            # Sistema imprime estado atual.
-        # Extensões: 
-            # 1a. Se o modo for automático, imprime: “Não é possível alterar a velocidade” e imprimir o estado atual do aparelho.
-            # 1b. Se a velocidade for maior que 5, imprimir “Atingiu a velocidade máxima” e imprimir o estado atual do aparelho.
-            # vel_min, vel_max
-    def diminuir_velocidade():
-        pass
-        # Fluxo normal: 
-            # Sistema decrementa a velocidade em 1.
-            # Sistema imprime estado atual.
-        # Extensões: 
-            # 1a 1a. Se o modo for automático, imprime: “Não é possível alterar a velocidade” e imprimir o estado atual do aparelho.
-            # 1b Se a velocidade for menor que 1, imprimir “Atingiu a velocidade mínima” e imprimir o estado atual do aparelho.
-    def mudar_modo(modo):
-        pass
-        # Fluxo normal: 
-            # Sistema pergunta qual modo o usuário deseja e apresenta as opções.
-            # Sistema altera o modo do aparelho
-            # Sistema imprime estado atual.
-        # Extensões: 
-            # 2a. Se o cliente escolher a opção 1, imprime “Frio”.
-            # 3b. Se o cliente escolher a opção 2, imprime “Ventilar”.
-            # 3c. Se o cliente escolher a opção 3, imprime “Automático”
-            # 3d. Se o cliente escolher a opção 4, imprime “Opção inválida”.
+    def operacao (self):
+        op_operacao = int(input(f'Informe a operação desejada:\n   1 - Sair\n   2 - Ligar o aparelho\n   3 - Desligar o aparelho\n   4 - Aumentar a temperatura\n   5 - Diminuir a temperatura\n   6 - Aumentar a velocidade\n   7 - Diminuir a velocidade\n   8 - Mudar o modo\n'))
+        # Sistema executa a operação desejada.
+        if op_operacao == 1:
+            print (self)
+        elif op_operacao == 2:
+            self.ligar()
+        elif op_operacao == 3:
+            self.desligar()
+        elif op_operacao == 4:
+            self.aumentar_temperatura()
+        elif op_operacao == 5:
+            self.diminuir_temperatura()
+        elif op_operacao == 6:
+            self.aumentar_velocidade()
+        elif op_operacao == 7:
+            self.diminuir_velocidade()
+        elif op_operacao == 8:
+            self.mudar_modo()
+        else:
+            raise ValueError ('Opção inválida.')
+    
+    def ligar(self):
+        #Se o aparelho estiver ligado, imprimir “Aparelho já está ligado” e imprimir o estado atual do aparelho
+        if self.ligado == True:
+            print ('Aparelho já está ligado\n')
+        else: 
+            self.ligado = True
+        # Sistema imprime estado atual
+        print(self)
+        return self.operacao()   
+        
+    def desligar(self):
+        #Se o aparelho estiver desligado, imprimir “Aparelho já está desligado” e imprimir o estado atual do aparelho
+        if self.ligado == True:
+            self.ligado = False
+        else: 
+            print ('Aparelho já está desligado.\n')
+        # Sistema imprime estado atual
+        print(self)
+        return self.operacao() 
+    
+    def aumentar_temperatura(self):
+        # Sistema pergunta quantos graus deseja aumentar.
+        temp = int(input ('Em quantos graus deseja aumentar a temperatura?\n'))
+        # Sistema incrementa a temperatura na quantidade de graus informada.
+        self.temperatura += temp
+        # Se a temperatura for maior que 28°C, imprimir “Atingiu temperatura máxima” e imprimir o estado atual do aparelho.
+        if self.temperatura > 28:
+            print ('O aparelho atingiu a temperatura máxima (28°C).\n')
+            self.temperatura = 28
+            print (self)
+        # Sistema imprime estado atual.
+        else: print (self)
+        return self.operacao()
+    
+    def diminuir_temperatura(self):
+        temp = int(input ('Em quantos graus deseja diminuir a temperatura?\n'))
+        self.temperatura -= temp
+        if self.temperatura < 18:
+            print ('O aparelho atingiu a temperatura mínima (18°C).\n')
+            self.temperatura = 18
+            print (self)
+        # Sistema imprime estado atual.
+        else: print (self)
+        return self.operacao()
+    
+    def aumentar_velocidade(self):
+        # Sistema incrementa a velocidade em 1.
+        self.velocidade += 1
+        # Se a velocidade for maior que 5, imprimir “Atingiu velocidade máxima” e imprimir o estado atual do aparelho.
+        if self.velocidade > 5:
+            print ('O aparelho atingiu a velocidade máxima (5).\n')
+            self.velocidade = 5
+            print (self)
+        # Sistema imprime estado atual.
+        else: print (self)
+        return self.operacao()
+    
+    def diminuir_velocidade(self):
+        # Sistema decrementa a velocidade em 1.
+        self.velocidade -= 1
+        # Se a velocidade for menor que 1, imprimir “Atingiu velocidade mínima e imprimir o estado atual do aparelho.
+        if self.velocidade <1:
+            print ('O aparelho atingiu a velocidade máxima (5).\n')
+            self.velocidade = 1
+            print (self)
+        # Sistema imprime estado atual.
+        else: print (self)
+        return self.operacao()
+    
+    def mudar_modo(self):
+        op_modo = int(input(f'Informa a opção desejada:\n   1 - Frio\n   2 - Ventilar\n   3 - Automático\n'))
+        if op_modo == 1:
+            self.modo = 'Frio'
+        elif op_modo == 2:
+            self.modo = 'Ventilar'
+        elif op_modo == 3:
+            self.modo = 'Automático'
+        else:
+            raise ValueError ('Opção Inválida\n')
+        print (self)
+        self.operacao()
+        
 def main():
     aparelho = ArCondicionado()
+    # Imprimir estado do aparelho
     print (aparelho)
-# Imprimir estado
-    # Fluxo normal: 
-        # Sistema lê o estado atual do aparelho
-        # Sistema imprime o estado atual do aparelho
-        # Sistema pergunta o que o usuário deseja fazer e apresenta opções.
-    # Extensões: 
-        # 3a. Se o cliente escolher a opção 1, encerrar o programa.
-        # 3b. Se o cliente escolher a opção 2, ligar o aparelho.
-        # 3c. Se o cliente escolher a opção 3, desligar o aparelho
-        # 3d. Se o cliente escolher a opção 4, aumentar a temperatura.
-        # 3e. Se o cliente escolher a opção 5, diminuir a temperatura
-        # 3f. Se o cliente escolher a opção 6, aumentar a velocidade.
-        # 3g. Se o cliente escolher a opção 7, diminuir a velocidade.
-        # 3h. Se o cliente escolher a opção 8, mudar o modo.
-
+    
+    # Sistema pergunta o que o usuário deseja fazer e apresenta opções.
+    aparelho.operacao()
+    
 if __name__=='__main__':
     main()
