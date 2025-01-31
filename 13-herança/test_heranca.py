c78ac9919436078f8cd2a0397352be75105f2cef
angelachantal <159492508+angelachantal@users.noreply.github.com>
2025-01-30 21:26:40 -0300

    Update test_heranca.py

--- 13-herança/test_heranca.py
+++ 13-herança/test_heranca.py
@@ -1,10 +1,12 @@
 import pytest
 from heranca import *
-
-def tnome():
+    
+def testarContaCorrente():
     conta = ContaCorrente(1234, 20000)
-    assert conta.ceditar() == ""
-    assert conta.debitar() == ""
-    assert conta.transferir() == None
-    assert conta.saldo() == None
-    assert str(conta) == f'Conta Corrente: {}'
+    assert str(conta) == 'Conta: 1234  Saldo: R$ 20000.00' #correto
+    assert conta.saldo + 100 == conta.creditar(100)
+    assert conta.saldo - 100 == conta.debitar(100)
+    # assert conta.transferir(100) == 
+    # assert conta.saldo == 
+    
+#Executar pytest test_heranca.py
