b8d7cf129fcf226e140e74c62d267fc0d1fa88d2
angelachantal <159492508+angelachantal@users.noreply.github.com>
2025-01-30 22:40:59 -0300

    Update heranca.py

--- 13-herança/heranca.py
+++ 13-herança/heranca.py
@@ -11,6 +11,10 @@ class ContaCorrente:
     def numero(self):
         return self.__numero
     
+    @saldo.setter
+    def saldo(self, saldo):            
+        self.__saldo = saldo
+    
     def __str__(self):
         return f'Conta: {self.__numero}  Saldo: R$ {self.__saldo:.2f}'
     
@@ -36,28 +40,42 @@ class ContaCorrente:
 class ContaPoupanca(ContaCorrente):
     def __init__(self, numero, saldo, taxa_juros):
         super().__init__(numero, saldo)
-        pass
+        self.__taxa_juros = taxa_juros
 
+    @property
+    def taxa_juros(self):
+        return self.__taxa_juros
+        
     def __str__(self):
-        return super().__str__()
-
-    def renderJuros():
-    #O método renderJuros(...) da subclasse ContaPoupança aplica o percentual definido no parâmetro taxa_juros em cima do atributo saldo da classe ContaCorrente, atualizando-o.
-        pass
+        return f'{super().__str__()}    Taxa de Juros: {self.__taxa_juros}%'
+        
+    def renderJuros(self):
+    #O método renderJuros() da subclasse ContaPoupança aplica o percentual definido no parâmetro taxa_juros em cima do atributo saldo da classe ContaCorrente, atualizando-o.
+        tx = self.taxa_juros/100
+        rendimento = self.saldo * tx
+        self.saldo += rendimento
+        return self.saldo
 
 class ContaImposto (ContaCorrente):
     def __init__(self, numero, saldo, percentual_imposto):
         super().__init__(numero, saldo)
-        pass
-
+        self.__percentual_imposto = percentual_imposto
+        
+    @property
+    def percentual_imposto(self):
+        return self.__percentual_imposto
+        
     def __str__(self):
-        return super().__str__()
+        return f'{super().__str__()}    Imposto: {self.__percentual_imposto}%'
 
-    def calcula_Imposto():
-        #subtrai do saldo, o valor do próprio saldomultiplicado pelo percentual do imposto.
-        pass
+    def calcula_Imposto(self):
+        #subtrai do saldo, o valor do próprio saldo multiplicado pelo percentual do imposto.
+        tx_imp = self.percentual_imposto/100
+        imposto = self.saldo * tx_imp
+        self.saldo -= imposto
+        return self.saldo
     
-# def main():
+def main():
     # conta1 = ContaCorrente(1234, 20000)
     # conta2 = ContaCorrente(123, 10000)
     # print (conta1)
@@ -69,6 +87,13 @@ class ContaImposto (ContaCorrente):
     
     # conta1.creditar(100)
     # print (conta1)
-        
-# if __name__ == '__main__':
-    # main()
+       
+    # conta3 = ContaPoupanca(1235, 1000, 1.5) 
+    # conta3.renderJuros()
+    # print (conta3)
+    
+    conta4 = ContaImposto(1236, 1000, 1.5) 
+    conta4.calcula_Imposto()
+    print (conta4)
+if __name__ == '__main__':
+    main()
