4faf60c476ce2d9dc80375f3b490d4224f6973b6
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-02-09 21:52:33 -0300

    feat: planoPre inciado

--- avaliacao/gerenciamento_chips.py
+++ avaliacao/gerenciamento_chips.py
@@ -3,26 +3,28 @@
 ## from random import randint
 # fazer verificações
 
+
 class Operadoras:
     def __init__(self):
         self.__clientes_lista = []
         pass
 
-    def adicionar_cliente(self, cliente): 
+    def adicionar_cliente(self, cliente):
         self.__clientes_lista.append(cliente)
-        print(f'Cadastrando cliente {cliente.nome}')
+        print(f"Cadastrando cliente {cliente.nome}")
         # Cadastrando cliente João...
 
     def listar_clientes(self):
-        print('Clientes')
+        print("Clientes")
         for cliente in self.__clientes_lista:
-            print(f'- {cliente.nome}')
+            print(f"- {cliente.nome}")
         # Clientes:
         # - João
         # - Maria
 
     def __str__(self):
         pass
+
 
 class Clientes:
     def __init__(self, nome):
@@ -32,79 +34,154 @@ class Clientes:
     @property
     def nome(self):
         return self.__nome
-    
+
     @property
     def chips(self):
-        return self.__chips 
+        return self.__chips
 
-    def adicionar_chip(self, tipo, numero): 
-        chip = Chips(tipo, numero)
+    def adicionar_chip(self, chip):
         self.__chips_lista.append(chip)
-        print(f'Associando o chip {tipo}-pago (Número: {chip.numero}) ao cliente {self.__nome}')
-        # Associando chip pré-pago (Número: 11987654321) ao cliente João...
+        print(
+            f"Associando o chip {chip.tipo}-pago (Número: {chip.numero}) ao cliente {self.__nome}"
+        )
 
     def listar_chips(self):
-        chips = ''
+        chips = ""
         for chip in self.__chips_lista:
-            chips += f'- Número: {chip.numero} - Tipo: {chip.tipo}\n'
+            chips += f"- Número: {chip.numero} - Tipo: {chip.tipo}\n"
         return chips
 
     def __str__(self):
         nome = self.__nome
         chip = self.listar_chips()
-        return f'Cliente: {nome}\nChips:\n{chip}'
+        return f"Cliente: {nome}\nChips:\n{chip}"
+
 
 class Chips:
-    def __init__(self, tipo, numero):
+    def __init__(self, tipo, numero, cliente, plano):
         self.__tipo = tipo
         self.__numero = numero
-       
+        self.__plano = plano  # instancia de plano
+        self.__cliente = cliente
+        self.__saldo = 0
+
     @property
     def tipo(self):
         return self.__tipo
     
     @property
+    def plano(self):
+        return self.__plano
+
+    @property
     def numero(self):
         return self.__numero
 
-    def realizar_chamada(self, duracao): ...
-    def enviar_sms(self, quantidade): ...
-    def consumir_dados_internet(self, gb): ...
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, saldo):
+        self.__saldo = saldo
+
+    ## Métodos polimórficos
+    def realizar_chamada(self, duracao):
+        # Deduz o custo da chamada do saldo (R$ 0,50/min).
+        custo = 0.5 * duracao
+        self.__plano.realizar_chamada(self, duracao, custo)
+
+    def enviar_sms(self, quantidade):
+        # Deduz o custo dos SMS do saldo (R$ 0,30/SMS).
+        custo = 0.3 * quantidade
+        self.__plano.enviar_sms(self, quantidade, custo)
+
+    def consumir_dados_internet(self, gb):
+        # Deduz o custo do saldo (R$ 5,00/GB).
+        custo = 5 * gb
+        self.__plano.consumir_dados_internet(self, gb, custo)
+
+    def adicionar_saldo(self, saldo):
+        self.__saldo = saldo
+        print(f"Recarga de R$ {saldo:.2f} realizada com sucesso!")
+
+    def mostrar_saldo(self):
+        return f"{self.__saldo:.2f}"
 
     def __str__(self):
         # João (Pré-pago, Número: 11987654321) → Saldo: R$ 33,50
         pass
 
+
 class Planos:
+    def __init__():
+        ...
+
     def calcular_custo(self):
         pass
 
-    def __init__(self):
-        pass
-
     def __str__(self):
         pass
 
-class PlanosPre(Planos, Chips):
+
+class PlanosPre(Planos):
+    def __init__(self):
+        super().__init__()
+        self.__consumo_sms = 0
+        self.__consumo_chamada = 0
+        self.__consumo_internet = 0
+        self.__consumo_total = 0
+
+    def calcular_custo(self):
+        return (
+            f"Chamadas: R$ {self.__consumo_total}"
+            f"SMS: R$ {self.__consumo_total}"
+            f"Internet: R$ {self.__consumo_total}"
+            f"Consumo total: R$ {self.__consumo_total}"
+        )
+
+    def realizar_chamada(self, chip, duracao, custo):
+        # Pré-pago: Deduz o custo da chamada do saldo (R$ 0,50/min).
+        chip.saldo -= custo
+        self.__consumo_chamada = custo
+        print(
+            f"Cliente {chip.cliente.nome} fez uma ligação de {duracao} minutos (custo: R$ {custo:.2f})..."
+        )
+        print(f"Saldo atual: {chip.mostrar_saldo()}")
+
+    def enviar_sms(self, chip, quantidade, custo):
+        chip.saldo -= custo
+        self.__consumo_sms = custo
+        print(
+            f"Cliente {chip.cliente.nome} enviou {quantidade} SMS (custo: R$ {custo:.2f})..."
+        )
+        print(f"Saldo atual: {chip.mostrar_saldo()}")
+
+    def consumir_dados_internet(self, chip, gb, custo):
+        chip.saldo -= custo
+        self.__consumo_internet = custo
+        print(
+            f"Cliente {chip.cliente.nome} consumiu  {gb}GB de internet (custo: R$ {custo:.2f})..."
+        )
+        print(f"Saldo atual: {chip.mostrar_saldo()}")
+
+    def __str__(self):
+        return super().__str__() + f"."
+
+
+class PlanosPos(Planos):
     def __init__(self):
         super().__init__()
         pass
 
     def calcular_custo(self): ...
-    def realizar_chamada(self, duracao): ...
-    def enviar_sms(self, quantidade): ...
-    def consumir_dados_internet(self, gb): ...
+    def realizar_chamada(self, duracao):
+        print("Chamada Pos")
 
-    def __str__(self):
-        return super(Planos).__str__() + f"."
-
-class PlanosPre(Planos, Chips):
-    def __init__(self):
-        super().__init__()
-        pass
-
-    def calcular_custo(self): ...
-    def realizar_chamada(self, duracao): ...
     def enviar_sms(self, quantidade): ...
     def consumir_dados_internet(self, gb): ...
 
--- avaliacao/teste_provisorio.py
+++ avaliacao/teste_provisorio.py
@@ -1,13 +1,26 @@
-from gerenciamento_chips import Operadoras, Clientes
+from gerenciamento_chips import *
 
 vivo = Operadoras()
 alice = Clientes('Alice')
 vivo.adicionar_cliente(alice)
 vivo.listar_clientes()
 
-# vivo.adicionar_cliente(alice)
+planoPre = PlanosPre()
+planoPos = PlanosPos()
+chip1 = Chips('PRE', '55 86 98826-0121', alice, planoPre)
+chip2 = Chips('POS', '55 86 98855-5555', alice, planoPos)
 
-alice.adicionar_chip('pos', '55 86 98826-0121')
-alice.adicionar_chip('pre', '55 86 98826657-0121')
-print(alice.listar_chips())
-print(alice)
+alice.adicionar_chip(chip1)
+alice.adicionar_chip(chip2)
+# alice.adicionar_chip('pre', '55 86 98826657-0121')
+# instanciar manualmente o chip, assim fica mais fácil acessar os métodos do chip
+# Ao adicionar um chip, passar uma instância do plano ou adicionar o plano no instanciamento de chip?
+# Ao chamar o chip.realizar_chamada(), o mesmo chama o método do plano ao qual herda
+
+chip1.adicionar_saldo(30)
+chip1.realizar_chamada(2)
+chip1.enviar_sms(3)
+chip1.consumir_dados_internet(4)
+print(chip1.plano.calcular_custo())
+# print(alice.listar_chips())
+# print(alice)
