bc79b96003ba994f06406d6b4186e2cf3c39e0ae
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-02-13 09:44:22 -0300

    feat: planosPos implementado e correções

--- avaliacao/gerenciamento_chips.py
+++ avaliacao/gerenciamento_chips.py
@@ -3,11 +3,23 @@
 ## from random import randint
 # fazer verificações
 
+# PENDÊNCIAS:
+# - Criar as exceções que faltam
+# - Código do pytest
+# - comentar código 
+# - datas - ok
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 class Operadoras:
-    def __init__(self):
+    def __init__(self, nome):
         self.__clientes_lista = []
-        pass
+        self.__nome = nome
+    
+    @property
+    def nome(self):
+        return self.__nome
 
     def adicionar_cliente(self, cliente):
         if cliente in self.__clientes_lista:
@@ -15,15 +27,11 @@ class Operadoras:
         else:
             self.__clientes_lista.append(cliente)
             print(f"Cadastrando cliente {cliente.nome}...")
-                    # Cadastrando cliente João...
 
     def listar_clientes(self):
         print("Clientes:")
         for cliente in self.__clientes_lista:
             print(f"- {cliente.nome}")
-            # Clientes:
-            # - João
-            # - Maria
 
     def __str__(self):
         return f'Operadora ativa.'
@@ -54,7 +62,7 @@ class Clientes:
     def listar_chips(self):
         chips = ""
         for chip in self.__chips_lista:
-            chips += f"- Número: {chip.numero} - Tipo: {chip.tipo}-pago\n"
+            chips += f"- Número: {chip.numero} - Tipo: {chip.plano.tipo}-pago\n"
         return chips
 
     def __str__(self):
@@ -66,8 +74,9 @@ class Clientes:
 class Chips:
     def __init__(self, numero, cliente, plano):
         self.__numero = numero
-        self.__plano = plano  # instancia de plano
+        self.__plano = plano
         self.__cliente = cliente
+        self.__cliente.adicionar_chip(self)
 
     @property
     def plano(self):
@@ -92,7 +101,6 @@ class Chips:
         self.__plano.consumir_dados_internet(self, gb)
 
     def __str__(self):
-        #Condicional para mostrar saldo ou valor da fatura
         return f'{self.__cliente.nome} ({self.__plano.tipo}-pago, Número: {self.__numero}) → Saldo: R$ {self.__plano.consumo_total}'
 
 
@@ -155,6 +163,14 @@ class Planos:
     def consumir_dados_internet(self):
         pass
 
+    def adicionar_saldo(self):
+        print('Saldo só está disponível em planos pré-pagos!')
+
+    def mostrar_saldo(self):
+        print('Saldo só está disponível em planos pré-pagos!')
+
+    def set_vencimento(self):
+        print('Só é possível mudar o vencimento em planos pós-pagos!')
     def __str__(self):
         pass
 
@@ -168,14 +184,6 @@ class PlanosPre(Planos):
     @property
     def tipo(self):
         return self.__tipo
-    
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @saldo.setter
-    def saldo(self, saldo):
-        self.__saldo = saldo
 
     def calcular_custo(self):
         self.consumo_total = (self.consumo_sms + self.consumo_chamada + self.consumo_internet)
@@ -185,50 +193,52 @@ class PlanosPre(Planos):
             f"SMS: R$ {self.consumo_sms:.2f}\n"
             f"Internet: R$ {self.consumo_internet:.2f}\n"
             f"------------------------\n"
-            f"Consumo total: R$ {self.consumo_total:.2f}"
+            f"Consumo total: R$ {self.consumo_total:.2f}\n"
+            f"Saldo atual: R$ {self.mostrar_saldo()}"
         )
 
     def consultar_status(self):
         return f'Saldo: {self.mostrar_saldo()}'
         
     def adicionar_saldo(self, saldo):
-        self.__saldo = saldo
+        self.__saldo += saldo
         print(f"Recarga de R$ {saldo:.2f} realizada com sucesso!")
 
     def mostrar_saldo(self):
         return f"{self.__saldo:.2f}"
 
-    def __verificar_saldo():
-        ...
+    def __verificar_saldo(self, custo):
+        if self.__saldo < custo:
+            raise ValueError(f'Saldo insuficiente!\nCusto: R$ {custo}\nSaldo atual: R$ {self.mostrar_saldo()}')
 
     def realizar_chamada(self, chip, duracao):
-        self.__verificar_saldo()
         # Pré-pago: Deduz o custo da chamada do saldo (R$ 0,50/min).
         custo = 0.5 * duracao
-        self.saldo -= custo
-        self.consumo_chamada = custo
+        self.__verificar_saldo(custo)
+        self.__saldo -= custo
+        self.consumo_chamada += custo
         print(
             f"Cliente {chip.cliente.nome} fez uma ligação de {duracao} minutos (custo: R$ {custo:.2f})..."
         )
         print(f"Saldo atual: {self.mostrar_saldo()}")
 
     def enviar_sms(self, chip, quantidade):
-        self.__verificar_saldo()
         # Deduz o custo dos SMS do saldo (R$ 0,30/SMS).
         custo = 0.3 * quantidade
-        self.saldo -= custo
-        self.consumo_sms = custo
+        self.__verificar_saldo(custo)
+        self.__saldo -= custo
+        self.consumo_sms += custo
         print(
             f"Cliente {chip.cliente.nome} enviou {quantidade} SMS (custo: R$ {custo:.2f})..."
         )
         print(f"Saldo atual: {self.mostrar_saldo()}")
 
     def consumir_dados_internet(self, chip, gb):
-        self.__verificar_saldo()
         # Deduz o custo do saldo (R$ 5,00/GB).
         custo = 5 * gb
-        self.saldo -= custo
-        self.consumo_internet = custo
+        self.__verificar_saldo(custo)
+        self.__saldo -= custo
+        self.consumo_internet += custo
         print(
             f"Cliente {chip.cliente.nome} consumiu  {gb}GB de internet (custo: R$ {custo:.2f})..."
         )
@@ -243,23 +253,25 @@ class PlanosPos(Planos):
         super().__init__()
         self.__tipo = 'pós'
         self.__fatura = 120
-        self.__vencimento = 0
+        self.__data_criacao = datetime.today()
+        self.__vencimento = self.__data_criacao + relativedelta(months=1)
+        
+        print(f'Fatura mensal de R$ {self.__fatura} gerada.')
     
-    @property
-    def tipo(self):
-        return self.__tipo
-    
-    @property
-    def fatura(self):
-        return self.__fatura
-    
-    @property
-    def fatura(self):
-        return self.__vencimento
-    
-    def __verificar_vencimento():
-        ...
+    def set_vencimento(self, vencimento):
+        try:
+            vencimento_valido = datetime.strptime(vencimento, "%d/%m/%Y")
+            self.__vencimento = vencimento_valido
+            print(f'Vencimento da fatura alterada para: {self.__vencimento}')
+        except ValueError:
+            raise ValueError("Data e hora inválidas!")
 
+    def __verificar_vencimento(self):
+        if datetime.today() < self.__vencimento:
+            return True
+        else:
+            raise ValueError(f"Fatura vencida!\nData do vencimento: {self.__vencimento}")
+        
     def calcular_custo(self):
         self.consumo_total = (self.consumo_sms + self.consumo_chamada + self.consumo_internet)
         return (
@@ -270,12 +282,11 @@ class PlanosPos(Planos):
             f"------------------------\n"
             f"Consumo total: R$ {self.consumo_total:.2f}\n"
             f'Fatura: R$ {self.__fatura:.2f}\n'
-            f'Vencimento: {self.__vencimento}'
+            f'Data criação: {self.__data_criacao}\n'
+            f'Vencimento: {self.__vencimento}\n'
         )
 
     def consultar_status(self):
-        # if data_atrasada: print('Valor da fatura; data de vencimento; Fatura em atraso.')
-        # else: 
         return f'Fatura: R$ {self.__fatura:.2f}\nVencimento: {self.__vencimento}'
         
     def realizar_chamada(self, chip, duracao):
--- avaliacao/teste_provisorio.py
+++ avaliacao/teste_provisorio.py
@@ -1,33 +1,50 @@
 from gerenciamento_chips import *
 
-vivo = Operadoras()
-alice = Clientes('Alice')
+## Criando operadora
+vivo = Operadoras(
+    nome="Vivo",
+)
+
+## Criando cliente
+alice = Clientes(
+    nome="Alice",
+)
+
+## Adicionando clientes a operadora
 vivo.adicionar_cliente(alice)
 vivo.listar_clientes()
 
-# É possível passar diretamente uma instância sem atribuir ela à uma variável
-chip1 = Chips('55 86 98826-0121', alice, PlanosPre())
-chip2 = Chips('55 86 98855-5555', alice, PlanosPos())
+## Adicionando chips a cliente
+chip1 = Chips(
+    numero="55 86 98826-0121",
+    cliente=alice,
+    plano=PlanosPre(),
+)
 
-alice.adicionar_chip(chip1)
-alice.adicionar_chip(chip2)
-# alice.adicionar_chip('pre', '55 86 98826657-0121')
-# instanciar manualmente o chip, assim fica mais fácil acessar os métodos do chip
-# Ao adicionar um chip, passar uma instância do plano ou adicionar o plano no instanciamento de chip?
-# Ao chamar o chip.realizar_chamada(), o mesmo chama o método do plano ao qual herda
+chip2 = Chips(
+    numero="55 86 98855-5555",
+    cliente=alice,
+    plano=PlanosPos(),
+)
 
-chip1.plano.adicionar_saldo(30)
-chip1.realizar_chamada(2)
-chip1.enviar_sms(3)
-chip1.consumir_dados_internet(4)
+## Listando chips de cliente
+print(alice.listar_chips())
 
+# print(chip2.plano.calcular_custo())
+
+# chip1.plano.adicionar_saldo(30)
+# chip1.realizar_chamada(2)
+# chip1.enviar_sms(3)
+# chip1.consumir_dados_internet(4)
+# chip1.plano.adicionar_saldo(11.9)
+# chip1.consumir_dados_internet(4)
 # print(chip1.plano.calcular_custo())
-# print(chip1.plano.consultar_status())
+# # print(chip1.plano.consultar_status())
 
+# chip2.plano.set_vencimento('12/01/2025')
 chip2.realizar_chamada(2)
 chip2.enviar_sms(3)
 chip2.consumir_dados_internet(4)
 print(chip2.plano.calcular_custo())
-print('--------------------')
-print(chip2.plano.consultar_status())
-# print(alice)
+chip1.plano.set_vencimento()
+# # print(alice)
