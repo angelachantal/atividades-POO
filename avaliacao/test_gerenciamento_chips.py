7139ae1e2b4808da7c2cc966967056baefc067ec
angelachantal <159492508+angelachantal@users.noreply.github.com>
2025-02-13 15:50:29 -0300

    feat: teste Operadoras

--- avaliacao/test_gerenciamento_chips.py
+++ avaliacao/test_gerenciamento_chips.py
@@ -2,63 +2,63 @@ import pytest
 from gerenciamento_chips import *
 
 #Executar pytest test_gerenciamento_chips.py
-def test_Operadoras():
-    operadora = Operadoras()
-    assert str(operadora) == 'Operadora ativa.'
 
-def test_adicionar_cliente(self, cliente):
-    # Entrada: João | Saída Esperada:  Cadastrando cliente João...
-    # Entrada: Maria | Saída Esperada:  Cadastrando cliente Maria...
-    # Entrada: João | Saída Esperada:  O cliente João já está cadastrado!
-    pass 
+def test_Operadoras(capfd):     #teste ok
+    vivo = Operadoras('Vivo')
+    assert str(vivo) == '''Operadora Vivo ativa.
+'''      #teste ok
 
-def test_listar_clientes(self):
-    # Entrada: operadora | Saída Esperada:  
-    # Clientes:
-    # - João
-    # - Maria
-    pass
-    
-def test_adicionar_chip(chip):
-    #Entrada para chip1 pré de João: 86999999999 | Saída esperada: Associando o chip pré-pago (Número: 86999999999) ao cliente João...
-    
-    #Entrada para chip2 pré de Maria: 86999999999 | Saída esperada: O número 86999999999 não está disponível..
-    
-    #Entrada para chip2 pré de Maria: 86988888888 | Saída esperada: Associando o chip pré-pago (Número: 86988888888) ao cliente Maria...
-    
-    #Entrada para chip3 pós de Maria: 86977777777 | Saída esperada: Associando o chip pos-pago (Número: 86977777777) ao cliente Maria...
-    pass
+    with pytest.raises(TypeError,match='Só é possível adicionar clientes válidos.'):
+        vivo.adicionar_cliente('João')      #teste ok
+        
+    joao = Clientes('João')
+    vivo.adicionar_cliente(joao)
+    out, err = capfd.readouterr() 
+    assert out == '''Cadastrando cliente João...
 
-def test_listar_chips():
-    # Entrada: Maria | Saída esperada: 
-    # - Número: {86988888888} - Tipo: pré-pago
-    # - Número: {86977777777} - Tipo: pós-pago
-    pass
+'''        #teste ok
+    
+    maria = Clientes('Maria')
+    vivo.adicionar_cliente(maria) 
+    out, err = capfd.readouterr()
+    assert out == '''Cadastrando cliente Maria...
 
-def test_Clientes():
-    cliente = Clientes('João')
-    assert str(cliente) == 'Cliente: João\nChips:86999999999'
+'''     #teste ok
 
-def test_realizar_chamada(duracao):
-    pass
-    # - Pré-pago?
-    # - Pós-pago?
-def test_enviar_sms(quantidade) :
-    pass
-    # - Pré-pago?
-    # - Pós-pago?
-def test_consumir_dados_internet(gb):
-    pass
-    # - Pré-pago?
-    # - Pós-pago?
+    vivo.adicionar_cliente(joao)
+    out, err = capfd.readouterr()
+    assert out == '''O cliente João já está cadastrado!
+
+'''        #teste ok
+         
+    vivo.listar_clientes()      #teste ok
+    out, err = capfd.readouterr()
+    assert out == '''Clientes:
+- João
+- Maria
+''' 
+    assert isinstance (joao, Clientes) == True      #teste ok
     
-def test_calcular_custo(meses) :
-    pass
-    # - Pré-pago?
-    # - Pós-pago?
+    assert isinstance (maria, Clientes) == True      #teste ok
     
-def test_Chips():
-    assert str(chip1) == 'João  (pré-pago, Número: 86999999999) → Saldo: R$ {self.__plano.consumo_total}'
+    assert isinstance (vivo, Operadoras) == True      #teste ok
+
     
-def test_Planos():
-    # assert str(chip1) == 'João  (pré-pago, Número: 86999999999) → Saldo: R$ {self.__plano.consumo_total}'
+def test_Clientes(capfd):
+    # vivo = Operadoras('Vivo')
+    # vivo.adicionar_cliente(joao)
+    joao = Clientes('João')     #teste ok
+    
+    chip1 = Chips(86999999999, PlanosPre(86999999999))      #teste ok
+    chip2 = Chips(86999999999, PlanosPre(86999999999))      #teste ok
+    chip3 = Chips(86988888888, PlanosPre(86988888888))      #teste ok
+    chip4 = Chips(86977777777, PlanosPos(86977777777))      #teste ok
+	
+    assert isinstance (chip1, Chips) == True        #teste ok
+    assert isinstance (chip2, Chips) == True        #teste ok
+    assert isinstance (chip3, Chips) == True        #teste ok
+    assert isinstance (chip4, Chips) == True        #teste ok
+	
+    joao.adicionar_chip(chip1) 
+    out, err = capfd.readouterr()
+    assert out == 'Associando o chip pré-pago (Número: 86999999999) ao cliente João...'
